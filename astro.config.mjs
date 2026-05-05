dd880e2d49c9cd824e79608893e550a21aa604d9
Kevin Zieber <zieber.kevinr@gmail.com>
2026-05-05 14:01:33 -0700

    dependencies fixed and astro version bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,19 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import compress from "@playform/compress";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import AutoImport from "astro-auto-import";
+import compress from "@playform/compress";
 import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kzieber.github.io",
 	integrations: [
-		// example auto import component into blog post mdx files
-		AutoImport({
-			imports: [
-				// https://github.com/delucis/astro-auto-import
-				"@components/Admonition/Admonition.astro",
-			],
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: false,
+			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+			SVG: false, // astro-icon handles this
 		}),
-		mdx(),
 		icon({
 			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
 			// https://www.astroicon.dev/reference/configuration#include
@@ -36,14 +32,6 @@ export default defineConfig({
 					"calendar-event",
 				],
 			},
-		}),
-		sitemap(),
-		compress({
-			HTML: true,
-			JavaScript: true,
-			CSS: false,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
 		}),
 	],
 	vite: {
