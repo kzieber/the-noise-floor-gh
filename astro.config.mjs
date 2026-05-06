b4d7726e725c6ecdbb1c62791250fe4c54ee1f00
Kevin Zieber <zieber.kevinr@gmail.com>
2026-05-06 07:35:26 -0700

    adapter adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,50 +5,54 @@ import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 import netlify from "@astrojs/netlify";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kzieber.github.io",
-	env: {
-		schema: {
-			RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
-		},
-	},
-	integrations: [
-		compress({
-			HTML: true,
-			JavaScript: true,
-			CSS: false,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
-		}),
-		icon({
-			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
-			// https://www.astroicon.dev/reference/configuration#include
-			include: {
-				tabler: [
-					"bulb",
-					"alert-triangle",
-					"flame",
-					"info-circle",
-					"arrow-narrow-left",
-					"arrow-narrow-right",
-					"menu-2",
-					"x",
-					"chevron-down",
-					"category",
-					"calendar-event",
-				],
-			},
-		}),
-	],
+    site: "https://kzieber.github.io",
+    env: {
+        schema: {
+            RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
+        },
+    },
+    integrations: [
+        compress({
+            HTML: true,
+            JavaScript: true,
+            CSS: false,
+            Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+            SVG: false, // astro-icon handles this
+        }),
+        icon({
+            // I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
+            // https://www.astroicon.dev/reference/configuration#include
+            include: {
+                tabler: [
+                    "bulb",
+                    "alert-triangle",
+                    "flame",
+                    "info-circle",
+                    "arrow-narrow-left",
+                    "arrow-narrow-right",
+                    "menu-2",
+                    "x",
+                    "chevron-down",
+                    "category",
+                    "calendar-event",
+                ],
+            },
+        }),
+    ],
 
-	vite: {
-		plugins: [tailwindcss()],
-		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
-		build: {
-			assetsInlineLimit: 0,
-		},
-	},
+    vite: {
+        plugins: [tailwindcss()],
+        // stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
+        build: {
+            assetsInlineLimit: 0,
+        },
+    },
 
-	adapter: netlify(),
+    adapter: node({
+      mode: "standalone",
+    }),
 });
