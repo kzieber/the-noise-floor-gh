241599ff95a20006e66c45501713464499968d4f
Kevin Zieber <zieber.kevinr@gmail.com>
2026-07-23 17:44:00 -0700

    package upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
-import compress from "@playform/compress";
+
 import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 import node from "@astrojs/node";
@@ -9,14 +9,6 @@ import node from "@astrojs/node";
 export default defineConfig({
 	site: "https://www.thenoisefloorjt.net/",
 	integrations: [
-		compress({
-			HTML: true,
-			JavaScript: true,
-			CSS: false,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
-		}),
-
 		icon({
 			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
 			// https://www.astroicon.dev/reference/configuration#include
